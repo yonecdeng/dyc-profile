c85fd2adae3bdd2472a7340d68a5cb33434ee17e
dengyongchi <dengyongchi@kuaishou.com>
2024-01-26 11:30:00 +0800

    feat: 添加vercel的性能监控

--- frontend/.vitepress/theme/index.ts
+++ frontend/.vitepress/theme/index.ts
@@ -25,7 +25,10 @@ DefaultTheme.enhanceApp = async ({ app, router, siteData }) => {
       tracesSampleRate: 1.0,
 
       // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      tracePropagationTargets: [
+        "localhost",
+        /^https:\/\/dyc-profile\.vercel\.app/,
+      ],
 
       // Capture Replay for 10% of all sessions,
       // plus for 100% of sessions with an error
